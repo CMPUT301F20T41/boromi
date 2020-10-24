5b64e41446d9db4fe51072d039c819bfb1c610d1
Andrew Chen <akchen@ualberta.ca>
2020-10-23 18:32:28 -0600

    reformatting

--- app/src/main/java/com/team41/boromi/book/GenericListFragment.java
+++ app/src/main/java/com/team41/boromi/book/GenericListFragment.java
@@ -12,54 +12,53 @@ import android.widget.TextView;
 import com.team41.boromi.R;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link GenericListFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A simple {@link Fragment} subclass. Use the {@link GenericListFragment#newInstance} factory
+ * method to create an instance of this fragment.
  */
 public class GenericListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "msg";
+  // TODO: Rename parameter arguments, choose names that match
+  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+  private static final String ARG_PARAM1 = "msg";
 
-    // TODO: Rename and change types of parameters
-    private String tempMsg;
+  // TODO: Rename and change types of parameters
+  private String tempMsg;
 
-    public GenericListFragment() {
-        // Required empty public constructor
+  public GenericListFragment() {
+    // Required empty public constructor
+  }
+
+  /**
+   * Use this factory method to create a new instance of this fragment using the provided
+   * parameters.
+   *
+   * @param param1 Parameter 1..
+   * @return A new instance of fragment OwnedAcceptedFragment.
+   */
+  // TODO: Rename and change types and number of parameters
+  public static GenericListFragment newInstance(String param1) {
+    GenericListFragment fragment = new GenericListFragment();
+    Bundle args = new Bundle();
+    args.putString(ARG_PARAM1, param1);
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (getArguments() != null) {
+      tempMsg = getArguments().getString(ARG_PARAM1);
     }
+  }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1..
-     * @return A new instance of fragment OwnedAcceptedFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GenericListFragment newInstance(String param1) {
-        GenericListFragment fragment = new GenericListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            tempMsg = getArguments().getString(ARG_PARAM1);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_generic_list, container, false);
-        TextView tempMsgView = view.findViewById(R.id.tempMessage);
-        tempMsgView.setText(tempMsg);
-        return view;
-    }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    View view = inflater.inflate(R.layout.fragment_generic_list, container, false);
+    TextView tempMsgView = view.findViewById(R.id.tempMessage);
+    tempMsgView.setText(tempMsg);
+    return view;
+  }
 }
