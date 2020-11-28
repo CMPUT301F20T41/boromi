e75b535d507d0a5bff279222c4b8ba8c322a6195
Brock Chelle <brocklchelle@gmail.com>
2020-11-27 17:35:08 -0700

    Maybe fixed?

--- app/src/main/java/com/team41/boromi/MainActivity.java
+++ app/src/main/java/com/team41/boromi/MainActivity.java
@@ -1,6 +1,9 @@
 package com.team41.boromi;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
       ft.add(R.id.auth_fragment, welcomeFragment).commit();
     }
 
+    this.createNotificationChannel();
+
 //    startActivity(new Intent(this, TestActivity.class));
   }
 
@@ -61,4 +66,19 @@ public class MainActivity extends AppCompatActivity {
     return authController;
   }
 
+  private void createNotificationChannel() {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      CharSequence name = getString(R.string.channel_name);
+      String description = getString(R.string.channel_description);
+      int importance = NotificationManager.IMPORTANCE_DEFAULT;
+      NotificationChannel channel = new NotificationChannel("postNotificationChannel", name, importance);
+      channel.setDescription(description);
+      // Register the channel with the system; you can't change the importance
+      // or other notification behaviors after this
+      NotificationManager notificationManager = getSystemService(NotificationManager.class);
+      notificationManager.createNotificationChannel(channel);
+    }
+  }
 }
