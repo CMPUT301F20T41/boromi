27da14dc64387c8ab5de624de390cdba8c7c758f
BWAGG <42561238+BWAGG@users.noreply.github.com>
2020-11-28 23:12:22 -0700

    Ben/updatehotfix (#145)
    * fixed needing to update borrow page
    
    * fixed regain bug

--- app/src/main/java/com/team41/boromi/utility/CustomClickListener.java
+++ app/src/main/java/com/team41/boromi/utility/CustomClickListener.java
@@ -8,7 +8,10 @@ import android.view.View;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import androidx.annotation.UiThread;
 import androidx.fragment.app.FragmentManager;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.team41.boromi.BookActivity;
 import com.team41.boromi.R;
 import com.team41.boromi.book.EditBookFragment;
@@ -16,6 +19,7 @@ import com.team41.boromi.book.GenericListFragment;
 import com.team41.boromi.callbacks.BookCallback;
 import com.team41.boromi.constants.CommonConstants;
 import com.team41.boromi.controllers.BookController;
+import com.team41.boromi.dbs.BookDB;
 import com.team41.boromi.models.Book;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -30,6 +34,8 @@ public class CustomClickListener implements View.OnClickListener,
   BookController bookController;
   BookActivity bookActivity;
   GenericListFragment genericListFragment;
+  BookDB bookDB;
+  FirebaseFirestore firestoreDb;
 
   public CustomClickListener(Book book, BookActivity bookActivity,
       GenericListFragment genericListFragment) {
@@ -100,6 +106,8 @@ public class CustomClickListener implements View.OnClickListener,
    */
   @Override
   public boolean onMenuItemClick(MenuItem menuItem) {
+    firestoreDb = FirebaseFirestore.getInstance();
+    bookDB = new BookDB (firestoreDb);
     switch (menuItem.getItemId()) {
       case R.id.edit_book:
         Log.d("edit!", book.getTitle());
@@ -128,7 +136,24 @@ public class CustomClickListener implements View.OnClickListener,
         });
         return true;
       case R.id.regain_book:
-        genericListFragment.verifyBarcode(book);
+        new Thread(new Runnable() {
+          @Override
+          public void run() {
+            Log.d("here", "we having good time");
+            if (bookDB.getBook(book.getBookId()).getWorkflow() == CommonConstants.BookWorkflowStage.PENDINGRETURN) {
+              Log.d("shouldnt be heree", "leave");
+              genericListFragment.verifyBarcode(book);
+            } else {
+              bookActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                  Log.d("this is where we want", "good");
+                  Toast.makeText(bookActivity, "Return Request Was Cancelled", Toast.LENGTH_LONG).show();
+                }
+              });
+            }
+          }
+        }).start();
         //genericListFragment.bookReturnRequest(book);
 
         return true;
